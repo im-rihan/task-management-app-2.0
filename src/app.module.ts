794ee25db851aae8eca62dffaaf0ae2505eda96f
Rihan Mohammed <rihanmohammed26@gmail.com>
2022-05-30 16:44:31 +0530

    added:config module & provide jwt secret from config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,19 +2,29 @@ import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { configValidationSchema } from './config.schema';
 
 @Module({
 	imports: [
+		ConfigModule.forRoot({
+			envFilePath: [`.env.stage.${process.env.STAGE}`],
+			validationSchema: configValidationSchema,
+		}),
 		TasksModule,
-		TypeOrmModule.forRoot({
-			type: 'mysql',
-			host: 'localhost',
-			port: 3306,
-			username: 'root',
-			password: '78626458',
-			database: 'task_management_2.0',
-			autoLoadEntities: true,
-			synchronize: true,
+		TypeOrmModule.forRootAsync({
+			imports: [ConfigModule],
+			inject: [ConfigService],
+			useFactory: async (configService: ConfigService) => ({
+				type: 'mysql',
+				autoLoadEntities: true,
+				synchronize: true,
+				host: configService.get('DB_HOST'),
+				port: configService.get('DB_PORT'),
+				username: configService.get('DB_USERNAME'),
+				password: configService.get('DB_PASSWORD'),
+				database: configService.get('DB_DATABASE'),
+			}),
 		}),
 		AuthModule,
 	],
